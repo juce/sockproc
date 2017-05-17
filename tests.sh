75fd043331d91081e8850729bedb91f67abb3c82
Anton Jouline <anton.jouline@cbsinteractive.com>
2017-05-17 10:01:29 -0700

    now tests.sh is really working again

--- tests.sh
+++ tests.sh
@@ -1,31 +1,33 @@
-#!/bin/sh -e
+#!/usr/bin/env bash
 #
 # Some unit-tests
-# (require 'nc' to be installed)
+# (require bash and nc to be installed)
+
+set -ue
 
 port=${1:-12345}
 pidfile=/tmp/sockproc-test.pid
 
 ./sockproc $port $pidfile
 
-echo "========================="
+echo -e "========================="
 
 # simple commands
-echo "uname -a\r\n0\r\n" | nc 127.0.0.1 $port
-echo "id\r\n0\r\n" | nc 127.0.0.1 $port
+echo -e "uname -a\r\n0\r\n" | nc 127.0.0.1 $port
+echo -e "id\r\n0\r\n" | nc 127.0.0.1 $port
 
 # commands with some input
-echo "wc -l\r\n12\r\nline1\r\nline2" | nc 127.0.0.1 $port
-echo "grep line\r\n20\r\nline1\r\nline2\r\nfoobar" | nc 127.0.0.1 $port
+echo -e "wc -l\r\n12\r\nline1\r\nline2" | nc 127.0.0.1 $port
+echo -e "grep line\r\n20\r\nline1\r\nline2\r\nfoobar" | nc 127.0.0.1 $port
 
 # bad command.expecting non-empty error stream
-echo "thisshouldfail\r\n0\r\n" | nc 127.0.0.1 $port
+echo -e "thisshouldfail\r\n0\r\n" | nc 127.0.0.1 $port
 
 # this should have data in both output and error streams
-echo "echo hello output && echo hello error >&2\r\n0\r\n" | nc 127.0.0.1 $port
+echo -e "echo hello output && echo hello error >&2\r\n0\r\n" | nc 127.0.0.1 $port
 
 # long command line strings
-echo "echo "\
+echo -e "echo "\
 "12345678901234567890123456789012345678901234567890"\
 "12345678901234567890123456789012345678901234567890"\
 "12345678901234567890123456789012345678901234567890"\
